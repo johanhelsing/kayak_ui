876d5a61c22ee1e540adee378d8ca06168818aa6
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2022-12-17 16:44:56 +0100

    Minimal version for toggling ui with tab

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,18 +1,76 @@
 use bevy::prelude::*;
 use kayak_ui::prelude::{widgets::*, *};
 
+#[derive(Component, Default, Clone, PartialEq, Eq)]
+pub struct MyWidgetProps {}
+
+impl Widget for MyWidgetProps {}
+
+#[derive(Bundle)]
+pub struct MyWidgetBundle {
+    props: MyWidgetProps,
+    styles: KStyle,
+    computed_styles: ComputedStyles,
+    widget_name: WidgetName,
+}
+
+impl Default for MyWidgetBundle {
+    fn default() -> Self {
+        Self {
+            props: Default::default(),
+            styles: Default::default(),
+            computed_styles: Default::default(),
+            widget_name: MyWidgetProps::default().get_name(),
+        }
+    }
+}
+
+// Our own version of widget_update that handles resource change events.
+pub fn widget_update_with_resource<
+    Props: PartialEq + Component + Clone,
+    State: PartialEq + Component + Clone,
+>(
+    In((widget_context, entity, previous_entity)): In<(KayakWidgetContext, Entity, Entity)>,
+    my_resource: Res<Input<KeyCode>>,
+    widget_param: WidgetParam<Props, State>,
+) -> bool {
+    widget_param.has_changed(&widget_context, entity, previous_entity) || my_resource.is_changed()
+}
+
 fn startup(
     mut commands: Commands,
     mut font_mapping: ResMut<FontMapping>,
     asset_server: Res<AssetServer>,
 ) {
-    font_mapping.set_default(asset_server.load("roboto.kayak_font"));
+    font_mapping.set_default(asset_server.load("roboto.kttf"));
 
     let mut widget_context = KayakRootContext::new();
     widget_context.add_plugin(KayakWidgetsContextPlugin);
+    widget_context.add_widget_data::<MyWidgetProps, EmptyState>();
+    widget_context.add_widget_system(
+        MyWidgetProps::default().get_name(),
+        widget_update_with_resource::<MyWidgetProps, EmptyState>,
+        my_widget_render,
+    );
     let parent_id = None;
     rsx! {
         <KayakAppBundle>
+            <MyWidgetBundle />
+        </KayakAppBundle>
+    };
+
+    commands.spawn(UICameraBundle::new(widget_context));
+}
+
+fn my_widget_render(
+    In((widget_context, entity)): In<(KayakWidgetContext, Entity)>,
+    keys: Res<Input<KeyCode>>,
+    mut commands: Commands,
+) -> bool {
+    if keys.pressed(KeyCode::Tab) {
+        info!("rendering text widget");
+        let parent_id = Some(entity);
+        rsx! {
             <TextWidgetBundle
                 text={TextProps {
                     content: "Hello World".into(),
@@ -20,10 +78,12 @@ fn startup(
                     ..Default::default()
                 }}
             />
-        </KayakAppBundle>
-    };
+        };
+    } else {
+        info!("not rendering anything");
+    }
 
-    commands.spawn(UICameraBundle::new(widget_context));
+    true
 }
 
 fn main() {
